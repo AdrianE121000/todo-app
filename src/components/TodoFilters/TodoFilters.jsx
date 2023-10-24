9b42a206c5747a9243344c6b2e8d32ed14e1e3e8
AdrianE121000 <paracosa3s@gmail.com>
2023-10-23 21:18:52 -0400

    initial commit

---
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -0,0 +1,44 @@
+import { FilterButton } from './Filter.components';
+import { FilterButtonContainer } from './Filter.components';
+import { FiltersContainer } from './Filter.components';
+import { ItemsLeft } from './Filter.components';
+
+const TodoFilters = ({
+  total,
+  activeFilter,
+  showAllTodos,
+  showActiveTodos,
+  showCompletedTodos,
+  handleClearComplete,
+}) => {
+  return (
+    <FiltersContainer>
+      <ItemsLeft total={total} />
+      <FilterButtonContainer>
+        <FilterButton
+          action={() => showAllTodos()}
+          active={activeFilter}
+          filter='All'
+        />
+        <FilterButton
+          action={() => showActiveTodos()}
+          active={activeFilter}
+          filter='Active'
+        />
+        <FilterButton
+          action={() => showCompletedTodos()}
+          active={activeFilter}
+          filter='Completed'
+        />
+      </FilterButtonContainer>
+
+      <button
+        onClick={() => handleClearComplete()}
+        className='text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in-out'>
+        Clear Completed
+      </button>
+    </FiltersContainer>
+  );
+};
+
+export default TodoFilters;
